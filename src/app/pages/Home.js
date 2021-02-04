73bee0473de8c0725b2e1882beedb2415a013665
Engaz01 <azakaite@github.com>
2021-02-04 19:44:40 +0200

    Update pages changing them to functions instead of class components

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -1,62 +1,41 @@
-import React from "react";
-
 import Hero from "../components/Hero";
 import Divider from "../components/Divider";
 import Card from "../components/Card";
 import Button from "../components/Button";
+import useFetch from "../hooks/useFetch";
 
-class Home extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      movies: [],
-    };
-  }
+function Home({ favorites, toggleFavorite }) {
+  const { loading, payload: movies = [] } = useFetch(
+    "https://academy-video-api.herokuapp.com/content/free-items"
+  );
 
-  componentDidMount() {
-    fetch("https://academy-video-api.herokuapp.com/content/free-items")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        this.setState({ movies: data });
-      });
-  }
-
-  render() {
-    const { movies } = this.state;
-    const { favorites, toggleFavorite } = this.props;
-    return (
-      <>
-        <Hero />
-        <Divider />
-        <article className="content">
-          <section className="content__wrapper">
-            <div className="content__movies">
-              {movies.map((movie) => {
-                if (movies.length < 0) {
-                  return <p>Loading...</p>;
-                }
-
-                return (
-                  <Card
-                    key={movie.id}
-                    image={movie.image}
-                    title={movie.title}
-                    description={movie.description}
-                    id={movie.id}
-                    toggleFavorite={toggleFavorite}
-                    favorites={favorites}
-                  />
-                );
-              })}
-            </div>
-            <Button size="large">Get More Content</Button>
-          </section>
-        </article>
-      </>
-    );
-  }
+  return (
+    <>
+      <Hero />
+      <Divider />
+      <article className="content">
+        <section className="content__wrapper">
+          <div className="content__movies">
+            {loading && <p>Loading...</p>}
+            {movies.map((movie) => {
+              return (
+                <Card
+                  key={movie.id}
+                  image={movie.image}
+                  title={movie.title}
+                  description={movie.description}
+                  id={movie.id}
+                  toggleFavorite={toggleFavorite}
+                  favorites={favorites}
+                />
+              );
+            })}
+          </div>
+          <Button size="large">Get More Content</Button>
+        </section>
+      </article>
+    </>
+  );
 }
 
 export default Home;
